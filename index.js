66390eab1d3ef167cb380143828ea5a56ebdb3ff
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-03 16:38:01 +0600

    completed docker for production and development stage

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h2>Hi There, I am mahedi!!!!</h2>");
+app.get("/test", (req, res) => {
+  res.send("<h2>Hi There, I am mah!!</h2>");
 });
 
 const port = process.env.PORT || 3000;
