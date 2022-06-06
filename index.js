e1f44890d743463908261a456b788a612c286d34
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-06 09:22:44 +0600

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
-app.get("/test", (req, res) => {
-  res.send("<h2>Hi There, I am mah!!</h2>");
+const user = [
+  {
+    name: "Mahedi Hasan",
+    age: "21",
+    email: "mahedisr@gmail.com",
+  },
+  {
+    name: "Anwar Hossain",
+    age: "25",
+    email: "mahedisr2@gmail.com",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send(user);
 });
 
 const port = process.env.PORT || 3000;
